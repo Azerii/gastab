3ce40cbd686c5d2794650046ab23f3c9305a8dd3
praise <praiseadeoti99@gmail.com>
2021-03-11 12:04:10 -0800

    gastab

---
+++ js/app.js
@@ -0,0 +1,114 @@
+function myFunction() {
+  let x = document.getElementById("myInput");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+ let step1=document.querySelector(".first");
+ let step2=document.querySelector(".second");
+ let step3=document.querySelector(".third")
+ let detail=document.querySelector(".tick");
+ let detail2=document.querySelector(".back");
+ let detail3=document.querySelector(".next");
+ let detail4=document.querySelector(".tickshoe");
+
+ detail.addEventListener("click",()=>{
+   step1.classList.remove("step2")
+   step1.classList.add("step1")
+   step2.classList.remove("step1")
+   step2.classList.add("step2")
+ })
+ detail2.addEventListener("click",()=>{
+  step2.classList.remove("step2")
+  step2.classList.add("step1")
+  step1.classList.add("step2")
+})
+detail3.addEventListener("click",()=>{
+  step2.classList.remove("step2")
+  step2.classList.add("step1")
+  step3.classList.remove("step1")
+  step3.classList.add("step2")
+})
+detail4.addEventListener("click",()=>{
+  step3.classList.remove("step2")
+  step3.classList.add("step1")
+  step2.classList.remove("step1")
+  step2.classList.add("step2")
+})
+
+
+
+document.querySelectorAll(".drop-zone__input").forEach((inputElement) => {
+  const dropZoneElement = inputElement.closest(".drop-zone");
+
+  dropZoneElement.addEventListener("click", (e) => {
+    inputElement.click();
+  });
+
+  inputElement.addEventListener("change", (e) => {
+    if (inputElement.files.length) {
+      updateThumbnail(dropZoneElement, inputElement.files[0]);
+    }
+  });
+
+  dropZoneElement.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    dropZoneElement.classList.add("drop-zone--over");
+  });
+
+  ["dragleave", "dragend"].forEach((type) => {
+    dropZoneElement.addEventListener(type, (e) => {
+      dropZoneElement.classList.remove("drop-zone--over");
+    });
+  });
+
+  dropZoneElement.addEventListener("drop", (e) => {
+    e.preventDefault();
+
+    if (e.dataTransfer.files.length) {
+      inputElement.files = e.dataTransfer.files;
+      updateThumbnail(dropZoneElement, e.dataTransfer.files[0]);
+    }
+
+    dropZoneElement.classList.remove("drop-zone--over");
+  });
+});
+
+/**
+ * Updates the thumbnail on a drop zone element.
+ *
+ * @param {HTMLElement} dropZoneElement
+ * @param {File} file
+ */
+function updateThumbnail(dropZoneElement, file) {
+  let thumbnailElement = dropZoneElement.querySelector(".drop-zone__thumb");
+
+  // First time - remove the prompt
+  if (dropZoneElement.querySelector(".drop-zone__prompt")) {
+    dropZoneElement.querySelector(".drop-zone__prompt").remove();
+  }
+
+  // First time - there is no thumbnail element, so lets create it
+  if (!thumbnailElement) {
+    thumbnailElement = document.createElement("div");
+    thumbnailElement.classList.add("drop-zone__thumb");
+    dropZoneElement.appendChild(thumbnailElement);
+  }
+
+  thumbnailElement.dataset.label = file.name;
+
+  // Show thumbnail for image files
+  if (file.type.startsWith("image/")) {
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      thumbnailElement.style.backgroundImage = `url('${reader.result}')`;
+    };
+  } else {
+    thumbnailElement.style.backgroundImage = null;
+  }
+}
+
---
+++ js/script.js
@@ -0,0 +1,64 @@
+let option=document.querySelector(".using");
+let delete2=document.querySelector(".edit1");
+option.addEventListener("click",()=>{
+    delete2.classList.toggle("step2")
+  })
+
+  let option1=document.querySelector(".using2");
+let delete3=document.querySelector(".edit2");
+option1.addEventListener("click",()=>{
+    delete3.classList.toggle("step2")
+  })
+
+  let option2=document.querySelector(".using3");
+let delete4=document.querySelector(".edit3");
+option2.addEventListener("click",()=>{
+    delete4.classList.toggle("step2")
+  })
+
+  let option3=document.querySelector(".using4");
+let delete5=document.querySelector(".edit4");
+option3.addEventListener("click",()=>{
+    delete5.classList.toggle("step2")
+  })
+
+
+  
+
+  let modalbutton=document.querySelector(".btn-m");
+let modalbg=document.querySelector(".modal-bg");
+let modalclose=document.querySelector(".close");
+//open the modal
+modalbutton.addEventListener("click",()=>{
+    modalbg.classList.add("bg-active")
+});
+//close the modal
+modalclose.addEventListener("click",()=>{
+    modalbg.classList.remove("bg-active")
+});
+
+ let modalbutton2=document.querySelector(".btn-m2");
+modalbutton2.addEventListener("click",()=>{
+  modalbg.classList.add("bg-active")
+});
+//close the modal
+modalclose.addEventListener("click",()=>{
+  modalbg.classList.remove("bg-active")
+});
+
+let modalbutton3=document.querySelector(".btn-m3");
+modalbutton3.addEventListener("click",()=>{
+  modalbg.classList.add("bg-active")
+});
+//close the modal
+modalclose.addEventListener("click",()=>{
+  modalbg.classList.remove("bg-active")
+});
+let modalbutton4=document.querySelector(".btn-m4");
+modalbutton4.addEventListener("click",()=>{
+  modalbg.classList.add("bg-active")
+});
+//close the modal
+modalclose.addEventListener("click",()=>{
+  modalbg.classList.remove("bg-active")
+});
