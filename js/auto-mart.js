c47c9a1697e7f0e309defb7e7739c1b91f3b7cf2
Odinaka Ezennia <ogidi58@gmail.com>
2021-03-21 20:29:20 +0100

    complete preview and UI fixes

---
+++ js/auto-mart.js
@@ -0,0 +1,167 @@
+const API_HOST = 'https://gastab-server.herokuapp.com';
+let listings = []
+
+const getCollection = async (collectionType, id) => {
+  // const token = getToken();
+  const options = {
+    method: 'get',
+    mode: 'cors',
+  };
+
+  if(id) options.body = id;
+
+  let res;
+  const url = id ? `${API_HOST}/${collectionType}/${id}` : `${API_HOST}/${collectionType}`;
+
+  try {
+    res = await fetch(url, options).then((response) => response.json());
+
+    if (res) {
+      if (res.length) {
+        const list = res;
+
+        localStorage.setItem(`${collectionType}List`, JSON.stringify(list));
+      }
+
+      return res;
+    }
+  } catch (e) {
+    console.log(e);
+  }
+
+  return null;
+};
+
+const formatNumber = (num) => {
+  const formatter = new Intl.NumberFormat()
+  const toNum = Number(num);
+
+  return formatter.format(toNum);
+};
+
+const setPreviewItemId = (id) => {
+  localStorage.setItem("previewItemId", id);
+}
+
+const list_element = document.querySelector('.card-ctn');
+
+const pagination_element = document.getElementById('pagination');
+
+let current_page = 1;
+
+let rows = 6;
+
+function DisplayList(items, wrapper, rows_per_page, page) {
+
+  wrapper.innerHTML = "";
+
+  page--;
+
+  let start = rows * page;
+
+  let end = start + rows_per_page;
+
+  let paginatedItems = items.slice(start, end)
+
+  
+
+  for (let i = 0; i < paginatedItems.length; i++){
+
+    let item = paginatedItems[i];
+
+    let item_element = document.createElement('div');
+    item_element.style.width = '100%'
+
+
+    item_element.innerHTML = `
+      <div class="card">
+        <a href = "./auto-preview.html" >
+          <img src="${API_HOST}${item.mainImage && (item.mainImage.url ?? "...")}" alt="${item.model}" onclick="setPreviewItemId('${item.id}')">
+        </a>
+        <div class="card-btm">
+          <p class="auto1-ft">
+            <span class="p-xmini">${item.condition}</span>
+            <span style="display: none;" class="p-xmini auto1-ft"><img src="./images/auto1-star.png" alt="auto1-star"> 4.8</span>
+          </p>
+          <p class="car-model">${item.model}</p>
+          <p class="car-price">₦&nbsp;${formatNumber(item.price)}</p>
+          <p class="p-xmini auto1-ft bottom-info">
+            <span class="metrics">
+              <span class="p-xmini car-mileage"><img src="./images/meter-logo.svg" alt="meter"> ${formatNumber(item.mileage)}</span> 
+              <span class="p-xmini car-location"><img src="./images/location-logo.svg" alt="location-logo"> ${item.location}</span>
+            </span>
+            <a href="./auto-preview.html" class="p-xmini go-to-preview">See more details</a>
+          </p>
+        </div>
+      </div>
+    `;
+    
+    wrapper.appendChild(item_element);
+
+  }
+}
+
+{/* <span class="p-xmini">See more
+          details</span></p> */}
+
+
+
+function SetupPagination(items, wrapper, rows_per_page) {
+
+  wrapper.innerHTML = ""
+  let page_count = Math.ceil(items.length / rows_per_page);
+
+  for (let i = 1; i < page_count + 1; i++){
+
+    let btn = PaginationButton(i,items)
+    
+
+    wrapper.appendChild(btn)
+
+  }
+  
+}
+
+
+
+function PaginationButton(page,items) {
+
+  let button = document.createElement('span');
+
+  button.innerText = page;
+
+  if (current_page == page) button.classList.add('active');
+
+
+  button.addEventListener('click', function () {
+    current_page = page;
+    DisplayList(items, list_element, rows, current_page)
+    
+    let current_btn = document.querySelector('.pagenumbers span.active')
+
+    current_btn.classList.remove('active')
+
+    button.classList.add('active')
+  });
+
+  return button
+
+}
+
+const showListings = () => {
+  DisplayList(listings, list_element,rows, current_page );
+  SetupPagination(listings, pagination_element,rows);
+}
+
+const setListings = async () => {
+  listings = await getCollection("vehicles");
+
+  if(listings && listings.length) {
+    showListings();
+  }
+};
+
+setListings();
+
+
+
---
+++ js/auto-preview.js
@@ -0,0 +1,258 @@
+const API_HOST = 'https://gastab-server.herokuapp.com';
+let vehicles;
+let previewItemId;
+let previewItem;
+
+if (localStorage.getItem("vehiclesList") && localStorage.getItem("previewItemId")) {
+    vehicles = JSON.parse(localStorage.getItem("vehiclesList"));
+    previewItemId = localStorage.getItem("previewItemId");
+
+    for (let i = 0; i < vehicles.length; i++) {
+        if (vehicles[i].id.toString() === previewItemId.toString()) {
+            previewItem = vehicles[i];
+            break;
+        }
+    }
+}
+
+let carousel = document.querySelector('.carousel');
+let leftArrow = document.querySelector('.left-arrow');
+let rightArrow = document.querySelector('.right-arrow');
+
+// const IMAGE_PAIR = [{
+//     bigSRC: './images/auto-truck.png',
+//     smallSRC: './images/auto-truck-sm.png'
+//   },
+//   {
+//     bigSRC: './images/auto-truck.png',
+//     smallSRC: './images/auto-truck-sm.png'
+//   },
+//   {
+//     bigSRC: './images/auto-truck.png',
+//     smallSRC: './images/auto-truck-sm.png'
+//   },
+//   {
+//     bigSRC: './images/auto-truck.png',
+//     smallSRC: './images/auto-truck-sm.png'
+//   },
+//   {
+//     bigSRC: './images/auto-truck.png',
+//     smallSRC: './images/auto-truck-sm.png'
+//   },
+//   {
+//     bigSRC: './images/auto-truck.png',
+//     smallSRC: './images/auto-truck-sm.png'
+//   },
+//   {
+//     bigSRC: './images/auto-truck.png',
+//     smallSRC: './images/auto-truck-sm.png'
+//   },
+//   {
+//     bigSRC: './images/auto-truck.png',
+//     smallSRC: './images/auto-truck-sm.png'
+//   },
+//   {
+//     bigSRC: './images/auto-truck.png',
+//     smallSRC: './images/auto-truck-sm.png'
+//   },
+//   {
+//     bigSRC: './images/auto-truck.png',
+//     smallSRC: './images/auto-truck-sm.png'
+//   },
+//   {
+//     bigSRC: './images/auto-truck.png',
+//     smallSRC: './images/auto-truck-sm.png'
+//   },
+//   {
+//     bigSRC: './images/auto-truck.png',
+//     smallSRC: './images/auto-truck-sm.png'
+//   },
+//   {
+//     bigSRC: './images/auto-truck.png',
+//     smallSRC: './images/auto-truck-sm.png'
+//   },
+//   {
+//     bigSRC: './images/auto-truck.png',
+//     smallSRC: './images/auto-truck-sm.png'
+//   },
+//   {
+//     bigSRC: './images/auto-truck.png',
+//     smallSRC: './images/auto-truck-sm.png'
+//   },
+//   {
+//     bigSRC: './images/auto-truck.png',
+//     smallSRC: './images/auto-truck-sm.png'
+//   },
+// ];
+
+// function printSixImages(start, end) {
+//   let newImagePair = IMAGE_PAIR.slice(start, end + 1);
+//   return newImagePair;
+// }
+
+
+
+
+// function toggleCarousel() {
+
+  // carousel.querySelector('.sm-img').innerHTML = `
+  //     ${
+  //       IMAGE_PAIR.map(
+  //         image => {
+            
+  //           return `
+  //           <img 
+  //           src="${image.smallSRC}" alt="${image.smallSRC.slice(10, -3)}"/>
+  //           `
+  //         }
+  //         ).join('')
+  //       }
+      
+  // `
+// }
+
+// toggleCarousel();
+
+const enlarge = document.querySelector('.enlarge');
+
+// Array.from(document.querySelectorAll('.sm-img img')).map(
+//   (el, index) => {
+//     el.setAttribute('onclick', `toggle('${IMAGE_PAIR[index].bigSRC}')`)
+//   }
+// );
+
+function toggle(src) {
+  enlarge.setAttribute('src', src)
+}
+const smImg = document.querySelector('.sm-img');
+
+const screen = document.querySelector('.screen');
+
+let index = 0;
+
+let width;
+
+
+function moveLeft() {
+  if (index > 0) {
+    index = index - 1
+    smImg.style.transform = `translateX(${-index * (width + 15)}px)`;
+  }
+}
+
+
+function moveRight() {
+  let slides = document.querySelector('.sm-img img')
+  if (index >= 0 && index < slides.length - 5) {
+    index = index + 1;
+    smImg.style.transform = `translateX(${-index * (width + 15)}px)`;
+  }
+}
+
+leftArrow.addEventListener('click', () => {
+  moveLeft()
+})
+rightArrow.addEventListener('click', () => {
+  moveRight()
+})
+
+const closeModal = (num) => {
+  ctModal[num].classList.remove('open');
+}
+
+const closeModalBtns = document.querySelectorAll('.closeModalBtn');
+
+const ctModal = document.querySelectorAll('.ct-modal');
+// closeModal(0)
+// closeModal(1)
+
+const btns = document.querySelectorAll('.auto2-sec1-b .btn');
+
+closeModalBtns[0].addEventListener('click',
+  () => closeModal(0)
+)
+closeModalBtns[1].addEventListener('click',
+  () => closeModal(1)
+)
+btns[0].addEventListener('click',
+  (event) => {
+    event.preventDefault()
+    // ctModal[0].classList.add('open');
+  }
+)
+btns[1].addEventListener('click',
+  (event) => {
+    event.preventDefault()
+    ctModal[1].classList.add('open');
+  }
+)
+
+const showPreview = () => {
+
+  const API_HOST = 'https://gastab-server.herokuapp.com';
+
+  const formatNumber = (num) => {
+      const formatter = new Intl.NumberFormat()
+      const toNum = Number(num);
+
+      return formatter.format(toNum);
+  };
+
+  // preview elements
+  const mainImage__preview = document.querySelector('.mainImage__preview');
+  const condition__preview = document.querySelector('.condition__preview');
+  const year__preview = document.querySelector('.year__preview');
+  const model__preview = document.querySelector('.model__preview');
+  const mileage__preview = document.querySelector('.mileage__preview');
+  const location__preview = document.querySelector('.location__preview');
+  const price__preview = document.querySelector('.price__preview');
+  const decsription__preview = document.querySelector('.description__preview');
+  const condition2__preview = document.querySelector('.condition2__preview');
+  const currentState__preview = document.querySelector('.currentState__preview');
+  const transmission__preview = document.querySelector('.transmission__preview');
+  const color__preview = document.querySelector('.color__preview');
+
+  // set preview image
+  if (previewItem && previewItem.mainImage) {
+      const previewUrl = previewItem.mainImage.url;
+      mainImage__preview.src = `${API_HOST}${previewUrl}`;
+  }
+
+  if (previewItem && previewItem.moreImages) {
+    carousel.querySelector('.sm-img').innerHTML = `
+      ${
+        previewItem.moreImages.map(
+          image => {
+            
+            return `
+            <img 
+            src="${API_HOST}${image.url}" alt="image slide"/>
+            `
+          }
+          ).join('')
+        }
+      
+  `
+  } else {
+    carousel.querySelector('.sm-img').innerHTML = `<p>-- --</p>`
+  }
+
+  // set preview details
+  if (previewItem) {
+      condition__preview.innerText = previewItem.condition;
+      year__preview.innerText = previewItem.year;
+      model__preview.innerText = previewItem.model;
+      mileage__preview.innerText = formatNumber(previewItem.mileage);
+      location__preview.innerText = previewItem.location;
+      price__preview.innerText = formatNumber(previewItem.price);
+      decsription__preview.innerText = previewItem.description;
+      condition2__preview.innerText = previewItem.condition;
+      currentState__preview.innerText = previewItem.currentState
+      transmission__preview.innerText = previewItem.transmission;
+      color__preview.innerText = previewItem.color;
+  }
+
+  width = document.querySelector('.sm-img img').width
+};
+
+showPreview();
